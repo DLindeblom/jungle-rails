faadd8b0f96b83a5bddf3f5aab81524c38a544e5
DLindeblom <drdlindeblom@gmail.com>
2022-08-01 21:55:40 +0000

    product validation testing - all tests pass

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+
+  before(:each) do
+    @category = Category.new
+    @product = Product.new(name:"Green plant", price_cents: 50, quantity: 10, category: @category)
+    @product.save!
+  end
+
+  describe "Validations" do
+
+      it "should save if all four fields have valid inputs" do
+        expect(Product.all.count).to eq(1)
+      
+      end
+
+      it "should not let a product be created if name is left blank" do
+        @product.name = nil
+        expect(@product).to_not be_valid
+      end
+
+      it "should not let a product be created if price is left blank" do
+        @product.price_cents = nil
+        expect(@product).to_not be_valid
+      end
+      
+      it "should not let a product be created if quantity is left blank" do
+        @product.quantity= nil
+        expect(@product).to_not be_valid
+      end   
+
+      it "should not let a product be created if category is left blank" do
+        @product.category = nil
+        expect(@product).to_not be_valid
+      end   
+
+    
+  end
+
+end
